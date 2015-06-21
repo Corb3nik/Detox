43fb156033b62660adc89201734c197345cd0a7b
Ian Bouchard <ian.bouchard@intello.com>
2015-06-21 00:04:38 -0400

    Adds issue model test

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Issue, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Issue, type: :model do
+  it "is invalid without a path" do
+    issue = Issue.new(filename: "test_filename", name: "test_name")
+    expect(issue.valid?).to be false
+  end
+
+  it "is invalid without a filename" do
+    issue = Issue.new(path: "test_path", name: "test_name")
+    expect(issue.valid?).to be false
+  end
+
+  it "is invalid without a name" do
+    issue = Issue.new(path: "test_path", filename: "test_filename")
+    expect(issue.valid?).to be false
+  end
 end
