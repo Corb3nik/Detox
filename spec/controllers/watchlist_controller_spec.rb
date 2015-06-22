0b4ecba99d8e79e8b02471aee43833eb161c364f
Ian Bouchard <ian.bouchard@intello.com>
2015-06-21 20:38:08 -0400

    Uses factorygirl in watchlist controller spec

--- spec/controllers/watchlist_controller_spec.rb
+++ spec/controllers/watchlist_controller_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 describe WatchlistController, type: :controller do
 
   before :each do
-    @comic_vine_series = ComicVineSeries.create name: "test_name", year: "test_year", status: "test_status"
-    @watched_issue = @comic_vine_series.watched_issues.create number: 1, status: "test_status"
+    @watched_issue = FactoryGirl.create :watched_issue
   end
 
   describe "GET #index" do
@@ -16,21 +15,21 @@ describe WatchlistController, type: :controller do
 
   describe "GET #show" do
     it "renders the show template" do
-      get :show, id: @comic_vine_series.id
+      get :show, id: @watched_issue.comic_vine_series_id
       expect(response).to render_template :show
     end
   end
 
   describe "GET #delete" do
     it "renders the delete template" do
-      get :delete, id: @comic_vine_series.id
+      get :delete, id: @watched_issue.comic_vine_series_id
       expect(response).to render_template :delete
     end
   end
 
   describe "DELETE #destroy" do
     it "redirects to #index" do
-      delete :destroy, id: @comic_vine_series.id
+      delete :destroy, id: @watched_issue.comic_vine_series_id
       expect(response).to redirect_to(:index)
     end
   end
