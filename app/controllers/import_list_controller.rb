77b52430bcd7f3242d0b9fcc9d8eb6dd4d68bf93
Ian Bouchard <ian.bouchard@intello.com>
2015-07-18 16:17:58 -0400

    Fixes failing specs and configures rspec

--- app/controllers/import_list_controller.rb
+++ app/controllers/import_list_controller.rb
@@ -5,16 +5,21 @@ class ImportListController < ApplicationController
   end
 
   def scan
-    @pending_issues = Scan.scan params[:location]
+    if params.has_key? :location
+      @pending_issues = Scan.scan params[:location]
+    end
+
     redirect_to :import_list
   end
 
   def import
-
-    for issue_id in params[:issue_ids] do
-      IssueManager.remove(issue_id, from: PendingIssue)
-      IssueManager.add(issue_id, to: WatchedIssue)
+    if params.has_key? :issue_ids
+      for issue_id in params[:issue_ids] do
+        IssueManager.remove(issue_id, from: PendingIssue)
+        IssueManager.add(issue_id, to: WatchedIssue)
+      end
     end
+
     redirect_to :import_list
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -89,4 +89,8 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  config.color = true
+  config.tty = true
+  config.formatter = :documentation
 end
