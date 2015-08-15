5bdcb3c9843455fa6aa086140232e95b2023ee89
Ian Bouchard <ian.bouchard@intello.com>
2015-08-15 14:11:56 -0400

    Implements updating of settings

--- app/controllers/configs_controller.rb
+++ app/controllers/configs_controller.rb
@@ -1,6 +1,16 @@
 class ConfigsController < ApplicationController
 
   def update
+    params[:settings].each do |key, value|
+      update_setting key, value
+    end
     redirect_to request.referer
   end
+
+  private
+
+  def update_setting(key, value)
+    Config.where(var: key).first.update(value: value)
+  end
+
 end
--- app/helpers/configs_helper.rb
+++ app/helpers/configs_helper.rb
@@ -1,2 +1,9 @@
 module ConfigsHelper
+  def setting_field(config)
+    if !!config.value == config.value
+      check_box_tag "settings[#{config.var}]", "1", config.value, class: 'pull-right'
+    elsif config.value.class == String
+      text_field_tag "settings[#{config.var}]", config.value, class: 'input-sm form-control'
+    end
+  end
 end
