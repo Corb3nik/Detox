70b71f3ff4872a8f3f1d76578166d291fe1f3c8c
Ian Bouchard <ian.bouchard@intello.com>
2015-09-17 14:36:47 -0400

    Adds working skip-previous links in match page

--- app/controllers/unmatched_list_controller.rb
+++ app/controllers/unmatched_list_controller.rb
@@ -5,5 +5,13 @@ class UnmatchedListController < ApplicationController
   end
 
   def match
+    if params[:current_id].nil? and URI(request.referer).path == unmatched_list_match_path
+      redirect_to action: :index
+      return
+    end
+
+    params[:current_id] ||= params[:issue_ids].first
+    @issue = Issue.find(params[:current_id])
+    @unmatched_entries = UnmatchedIssue.where(issue_id: @issue.id)
   end
 end
